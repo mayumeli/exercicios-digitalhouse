8cf367be2315974630a03496646af2f56270fbd5
mayumeli <mayu.satori@mercadolivre.com>
2022-10-13 18:39:37 -0300

    Exc. User Test: adc new controller test

--- modulo5/testing/usertest/src/test/java/com/usertest/mayumeli/usertest/controller/UserControllerTest.java
+++ modulo5/testing/usertest/src/test/java/com/usertest/mayumeli/usertest/controller/UserControllerTest.java
@@ -3,15 +3,21 @@ package com.usertest.mayumeli.usertest.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.usertest.mayumeli.usertest.dto.UserDto;
 import com.usertest.mayumeli.usertest.model.User;
 import com.usertest.mayumeli.usertest.service.UserService;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -22,17 +28,38 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private UserService service;
+
+    private User mayu;
+
+    @BeforeEach
+    void setup() {
+        mayu = new User(1, "Mayumi", "mayu@hotmail.com", "123456");
+    }
 
     @Test
     public void callingGetAllUsersShouldReturnAllUsers() throws Exception {
         List<User> users = new ArrayList<>();
-        users.add(new User(1, "Mayumi", "mayu@hotmail.com", "123456"));
+        users.add(mayu);
         BDDMockito.given(service.getAllUsers()).willReturn(users);
         
         this.mockMvc.perform(get("/api/v1/user/all"))
             .andExpect(status().isOk())
             .andExpect(content().json("[{'id': 1, 'name': 'Mayumi', 'email': 'mayu@hotmail.com'}]"));
+    }
+
+    @Test
+    public void callingCreateUserShouldReturnCreatedUser() throws Exception {
+        BDDMockito.given(service.createUser(any())).willReturn(new UserDto(mayu));
+
+        String json = objectMapper.writeValueAsString(mayu);
+        
+        this.mockMvc.perform(post("/api/v1/user/create").content(json).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andExpect(content().json("{'id': 1, 'name': 'Mayumi', 'email': 'mayu@hotmail.com'}"));
     }
 }
